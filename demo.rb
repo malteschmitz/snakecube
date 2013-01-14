c51a04e1963715fa742c0c7c22ac56d843d32d71
Eike von Tils <eike.vontils@informatik.uni-luebeck.de>
2013-01-14 09:50:40 +0100

    Adds snake_to_string method
    This method converts a bitvector and a X-I string to a R-L-I string.
    Currently the implementation is very ugly.

--- demo.rb
+++ demo.rb
@@ -1,7 +1,11 @@
 require_relative "evolution"
 
+# base string of snake
+@snake_static = "IXIIXIXXIXIXIXXIIXIXIXXIIXIXXIXIIXIXIIXXXIXIIXIXIXIIXIXXIIXIXI"
+
 # number of bits in the bitvector
 length = 30
+
 # fitness function that returns fitness of a bitvector
 # (the lower the number, the fitter the bitvector)
 fitness = lambda do |value|
@@ -10,6 +14,29 @@ fitness = lambda do |value|
   # return the number of zeros as fitness
   # (yes, this is a nonsense example!)
   s.count('0')
+end
+
+def snake_to_string(length, value)
+  configuration = "%.#{length}b" % value
+  last_direction = 'R'
+  config_index = 0
+  result = ""
+  @snake_static.each_char do |c|
+    if c != 'X'
+      result << c
+    else
+      if configuration[config_index] != '0'
+        if last_direction == 'R'
+          last_direction = 'L'
+        else
+          last_direction = 'R'
+        end
+      end
+      result << last_direction
+      config_index += 1
+    end
+  end
+  result
 end
 
 e = Evolution.new({
@@ -27,4 +54,4 @@ e.iterate({
   :fitness => 0
 })
 
-puts e.population.map{|i| i[:fitness].to_s + "\t" + i[:value].to_s}.join("\n")
+puts e.population.map{|i| i[:fitness].to_s + "  " + i[:value].to_s + "  " + snake_to_string(length, i[:value])}.join("\n")
