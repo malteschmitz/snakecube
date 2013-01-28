c9333b024152968aefa45458bdd6a398bc8174ce
Malte Schmitz <malte@schmitz-sh.de>
2013-01-28 16:37:50 +0100

    Improves output

--- snake_bruteforce.rb
+++ snake_bruteforce.rb
@@ -11,5 +11,27 @@ all = {}
     all[energy] = individual unless all.has_key?(energy)
   end
 end
+
+print_individual = lambda do |i|
+  s = snake.to_string.call(i[:value])
+  puts "%f\t%.#{snake.length}b\t%s" % [i[:energy], i[:value], s]
+end
+
 a = all.values.sort { |a,b| a[:energy] <=> b[:energy] }
-a.each { |i| puts "%f\t%.#{snake.length}b" % [i[:energy], i[:value]] }
+
+puts 'all energies and first found individual with that energy:'
+a.each { |i| print_individual.call(i) }
+puts ''
+puts 'first found fittest individual:'
+opt = a.first
+print_individual.call(opt)
+board = snake.to_board.call(snake.to_string.call(opt[:value]))
+puts ''
+puts board.map{|x| x.join("")}.join("\n")
+puts ''
+puts 'first found unfittest individual:'
+worst = a.last
+print_individual.call(worst)
+board = snake.to_board.call(snake.to_string.call(worst[:value]))
+puts ''
+puts board.map{|x| x.join("")}.join("\n")
--- snake_evolution.rb
+++ snake_evolution.rb
@@ -5,7 +5,7 @@ snake = Snake.new("IIXXXIXXIXXXIXIXXXXIXIXIXII")
 
 e = Evolution.new({
   :length => snake.length,
-  :energy => snake.energy_a,
+  :energy => snake.energy_c,
   :size => 15,
   :crossover => 5,
   :mutation => 15,
@@ -15,7 +15,7 @@ e = Evolution.new({
 
 e.iterate({
   :n => 10000,
-  :energy => 9,  # use 9 for energy_a, 1 for energy_b and -15 for energy_c
+  :energy => -15,  # use 9 for energy_a, 1 for energy_b and -15 for energy_c
   :logging => true
 })
 
@@ -28,11 +28,12 @@ puts "number of iterations:\n#{e.iterations}\n\n"
 puts 'final population:'
 e.population.each { |i| print_individual.call(i) }
 puts ''
-puts 'all energies:'
+puts 'all energies and first found individual with that energy:'
 e.individuals.each { |i| print_individual.call(i) }
 puts ''
-puts 'fittest individual:'
+puts 'fittest individual in final population:'
 opt = e.population.first
-puts print_individual.call(opt)
+print_individual.call(opt)
 board = snake.to_board.call(snake.to_string.call(opt[:value]))
+puts ''
 puts board.map{|x| x.join("")}.join("\n")
